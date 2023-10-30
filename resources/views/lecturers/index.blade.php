480d48a7d107038388f063b60ae51ac3a9fd75c1
rizutha <arifqiza@gmail.com>
2023-10-30 14:11:32 +0700

    ganti view part 1

--- resources/views/lecturers/index.blade.php
+++ resources/views/lecturers/index.blade.php
@@ -1,51 +1,53 @@
 @extends('components.layout')
 @section('title', 'Dosen')
-<div class="container-fluid">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <div class="row mb-2">
-        <div class="col-sm-6">
-            <h2 class="text-dark m-0">Data Dosen</h2>
-        </div>
-    </div>
-</div>
 @section('content')
-    <div class="box-header with-border">
-        <a href="{{ route('lecturers.create') }}" class="btn btn-success btn-xs btnflat"><i class="fa fa-plus-circle"></i>
-            Tambah Data
-        </a>
-    </div>
-    <br>
-    <div class="box-body table-responsive">
-        <table class="table-stiped table-bordered table">
-            <thead>
-                <tr>
-                    <th width="5%">No</th>
-                    <th>NIP</th>
-                    <th>Nama</th>
-                    <th>Keilmuan</th>
-                    <th width="20%"><i class="fas fa-cog">Action</i></th>
-                </tr>
-            </thead>
-            @foreach ($dosen as $item)
-                <tr>
-                    <td>{{ ++$i }}</td>
-                    <td>{{ $item->nip }}</td>
-                    <td>{{ $item->nama }}</td>
-                    <td>{{ $item->keilmuan }}</td>
-                    <td>
-                        <a href="{{ route('lecturers.show', [$item->id]) }}" class="btn btn-info btn-sm">Detail</a>
-                        <a href="{{ route('lecturers.edit', [$item->id]) }}" class="btn btn-primary btn-sm">Edit</a>
-                        <form class="d-inline" action="{{ route('lecturers.destroy', [$item->id]) }}" method="POST"
-                            onsubmit="return confirm('Yakin hapus data?')">
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-danger btn-sm" type="submit">Delete</button>
-                        </form>
+    <div class="rounded-4 p-5 shadow-lg">
+        <div class="">
+            <meta name="csrf-token" content="{{ csrf_token() }}">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h2 class="text-dark m-0">Data Dosen</h2>
+                </div>
+            </div>
+        </div>
+        <div class="box-header with-border">
+            <a href="{{ route('lecturers.create') }}" class="btn btn-success btn-xs btnflat"><i class="fa fa-plus-circle"></i>
+                Tambah Data
+            </a>
+        </div>
+        <br>
+        <div class="box-body table-responsive">
+            <table class="table-hover table">
+                <thead>
+                    <tr>
+                        <th width="5%">No</th>
+                        <th>NIP</th>
+                        <th>Nama</th>
+                        <th>Keilmuan</th>
+                        <th width="20%"><i class="fas fa-cog">Action</i></th>
+                    </tr>
+                </thead>
+                @foreach ($dosen as $item)
+                    <tr>
+                        <td>{{ ++$i }}</td>
+                        <td>{{ $item->nip }}</td>
+                        <td>{{ $item->nama }}</td>
+                        <td>{{ $item->keilmuan }}</td>
+                        <td>
+                            <a href="{{ route('lecturers.show', [$item->id]) }}" class="btn btn-info btn-sm">Detail</a>
+                            <a href="{{ route('lecturers.edit', [$item->id]) }}" class="btn btn-primary btn-sm">Edit</a>
+                            <form class="d-inline" action="{{ route('lecturers.destroy', [$item->id]) }}" method="POST"
+                                onsubmit="return confirm('Yakin hapus data?')">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+                            </form>
 
-                    </td>
-                </tr>
-            @endforeach
-        </table>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+        {{ $dosen->links() }}
     </div>
-    {{ $dosen->links() }}
 @endsection
